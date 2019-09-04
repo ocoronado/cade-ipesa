94c7da8c09a498bcd2d01808df127ec460d462ff
ocoronado <ocoronado@ipesamex.com>
2019-09-04 17:15:04 -0500

    Initial commit

---
+++ src/app/servicios/global.ts
@@ -0,0 +1,5 @@
+export var GLOBAL = {
+	//simepre va ir a buscar en esa direccion
+    url: 'https://frozen-eyrie-91455.herokuapp.com/api/',
+    urlSinApi: 'https://frozen-eyrie-91455.herokuapp.com/'
+}
